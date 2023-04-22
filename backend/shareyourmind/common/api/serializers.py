e36db05a2c0ee23a7d34a2f73db627338bb457f3
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-04-22 20:14:04 +0200

    [BACKEND] Adjust updating user

--- backend/shareyourmind/common/api/serializers.py
+++ backend/shareyourmind/common/api/serializers.py
@@ -4,6 +4,8 @@ from shareyourmind.common.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField()
+
     class Meta:
         model = Category
         fields = ("id", "name", "is_active")
