a09485d22bff688c73bc3dd8ee2dd4d52d1e26ed
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-04-23 15:57:15 +0200

    [BACKEND] Create command for creating categories

---
+++ backend/shareyourmind/common/management/commands/createcategories.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from shareyourmind.common.models import Category
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+
+        with open("shareyourmind/common/management/commands/categories.txt", mode="r") as categories:
+            for category in categories.readlines():
+                Category.objects.create(name=category)
+
+        print("Categories were successfully created!")
