551221066ee6c02120b161fa6b43d6bfe4becd11
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-05-07 17:46:02 +0200

    [BACKEND] Media files settings

--- backend/shareyourmind/settings.py
+++ backend/shareyourmind/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "static/"
 
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- backend/shareyourmind/urls.py
+++ backend/shareyourmind/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -62,3 +64,6 @@ urlpatterns = [
     ),
     path("api/v1/search/", SearchAPIView.as_view(), name="search"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
