d4ff92956ec371eb1a62f2f7a02c3297c7ef030d
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-05-07 16:54:37 +0200

    [BACKEND] Blog posts models

--- backend/shareyourmind/blog_posts/admin.py
+++ backend/shareyourmind/blog_posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from shareyourmind.blog_posts.models import BlogPost, BlogPostComment
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title")
+
+
+@admin.register(BlogPostComment)
+class BlogPostCommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "short_body")
--- backend/shareyourmind/blog_posts/models.py
+++ backend/shareyourmind/blog_posts/models.py
@@ -1,3 +1,34 @@
+from ckeditor.fields import RichTextField
 from django.db import models
+from django.template.defaultfilters import striptags
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+from shareyourmind.common.models import PublishedContentMixin, ObjectContentTypeMixin, CommentMixin
+
+
+class BlogPost(PublishedContentMixin, ObjectContentTypeMixin):
+    OBJECT_CONTENT_TYPE = "blog_post"
+
+    image = models.ImageField(upload_to="images")
+    title = models.TextField(max_length=120)
+    content = RichTextField()
+
+    categories = models.ManyToManyField("common.Category", related_name="blog_posts")
+
+    @property
+    def short_content(self):
+        if len(self.content.__str__()) > 100:
+            return striptags(mark_safe(self.content[:100] + "..."))
+        return striptags(mark_safe(self.content))
+
+    def __str__(self):
+        return f"{self.author}: {self.title}"
+
+
+class BlogPostComment(CommentMixin):
+    blog_post = models.ForeignKey(
+        "blog_posts.BlogPost", on_delete=models.CASCADE, related_name="comments"
+    )
+
+    def __str__(self):
+        return f"{self.author}: {self.short_body}"
