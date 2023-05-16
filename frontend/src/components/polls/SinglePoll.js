c7ab9c2c975655f3f7f8dbc987f88cc40aac4c26
Sebastian Jędrzejewski <sebastian.jedrzejewski@jmr.pl>
2023-05-16 22:47:21 +0200

    [FRONTEND] Nested comments to polls

--- frontend/src/components/polls/SinglePoll.js
+++ frontend/src/components/polls/SinglePoll.js
@@ -14,6 +14,10 @@ import useFetchUser from "../../hooks/useFetchUser";
 import DeleteModal, { showDeleteModal } from "../modals/DeleteModal";
 import { ContentLikes } from "../questions/SingleQuestion";
 import { Poll } from "./Polls";
+import { Tooltip } from "react-tooltip";
+import { useCollapse } from "react-collapsed";
+import up from "../../assets/up.png";
+import down from "../../assets/down.png";
 
 export const SinglePoll = () => {
   const { id } = useParams();
@@ -71,7 +75,6 @@ export const SinglePoll = () => {
               contentId: id,
             }}
           />
-
           <p className="answer-note">
             {comments?.length} {comments?.length === 1 ? "Comment" : "Comments"}
           </p>
@@ -85,21 +88,22 @@ export const SinglePoll = () => {
                       key={comment.id}
                       user={user}
                       comment={comment}
+                      pollId={id}
                       commentState={commentState}
                       setCommentState={setCommentState}
                     />
-                    {index !== comments?.length - 1 && (
-                      <>
-                        <br></br>
-                        <hr></hr>
-                      </>
-                    )}
+                    {index !== comments?.length - 1 &&
+                      comment?.parent_comment_id === null && (
+                        <>
+                          <br></br>
+                          <hr></hr>
+                        </>
+                      )}
                   </span>
                 );
               })}
             </div>
           )}
-
           <CommentField
             pollId={id}
             commentState={commentState}
@@ -171,12 +175,156 @@ export const EditDelete = ({
   );
 };
 
-export const Comment = ({ user, comment, commentState, setCommentState }) => {
-  const { id, created_at, updated_at, likes, author, body } = comment;
+export const Comment = ({
+  user,
+  comment,
+  pollId,
+  commentState,
+  setCommentState,
+}) => {
+  const {
+    id,
+    created_at,
+    updated_at,
+    likes,
+    author,
+    body,
+    parent_comment_id,
+    nested_comments,
+  } = comment;
+  const [showCommentField, setShowCommentField] = useState(false);
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
   const deleteComment = (id) => {
     apiCall
       .delete(`/api/v1/poll_comments/${id}/`)
+      .then(() => document.location.reload());
+  };
+
+  if (parent_comment_id) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-9 author">
+          {author?.username}{" "}
+          <span className="date">commented {getDateString(created_at)}</span>
+          {getDateString(created_at) !== getDateString(updated_at) && (
+            <span className="date">, modified {getDateString(updated_at)}</span>
+          )}
+        </div>
+        <div className="col-md-3 ref-answer">
+          <span
+            id={`answer-to-poll-comment${id}`}
+            style={{ cursor: "pointer" }}
+            data-tooltip-id="answer-tooltip"
+            data-tooltip-content="Answer to this comment"
+          >
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={(e) => {
+                setShowCommentField(!showCommentField);
+                if (showCommentField) {
+                  e.target.innerHTML = "Answer";
+                  document
+                    .getElementById(`answer-to-poll-comment${id}`)
+                    .setAttribute(
+                      "data-tooltip-content",
+                      "Answer to this comment"
+                    );
+                } else {
+                  e.target.innerHTML = "Cancel";
+                  document
+                    .getElementById(`answer-to-poll-comment${id}`)
+                    .setAttribute("data-tooltip-content", "Cancel answering");
+                }
+              }}
+            >
+              Answer
+            </button>
+          </span>
+          <Tooltip id="answer-tooltip" place="bottom" />
+        </div>
+      </div>
+      {body && (
+        <div
+          className="question-description answer-body"
+          style={{ fontSize: "Large", border: "none", marginTop: "1.5rem" }}
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+      )}
+      <ContentLikes
+        contentType={"poll_comment"}
+        contentText={"comment"}
+        initialState={likes}
+        contentId={id}
+      />
+
+      {user?.username === author?.username && (
+        <EditDelete
+          deleteModalId={"comment-delete-modal"}
+          deleteAction={deleteComment}
+          contentId={id}
+          commentState={commentState}
+          setCommentState={setCommentState}
+        />
+      )}
+
+      {showCommentField && (
+        <NestedCommentField pollId={pollId} commentId={id} />
+      )}
+
+      {nested_comments?.length > 0 && (
+        <div className="drop-answers" {...getToggleProps()}>
+          <span style={{ marginRight: "5px" }}>
+            <img width="30" src={isExpanded ? up : down} alt="up/down" />
+          </span>
+          {`${nested_comments?.length} answer` +
+            (nested_comments?.length > 1 ? "s" : "")}
+        </div>
+      )}
+
+      {nested_comments?.length > 0 && (
+        <div className="welcome-container question-box" {...getCollapseProps()}>
+          {nested_comments.map((comment, index) => {
+            return (
+              <>
+                <NestedComment
+                  key={comment.id}
+                  user={user}
+                  comment={comment}
+                  commentState={setCommentState}
+                  setCommentState={setCommentState}
+                />
+                {index !== nested_comments?.length - 1 && (
+                  <>
+                    <br></br>
+                    <hr></hr>
+                  </>
+                )}
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export const NestedComment = ({
+  user,
+  comment,
+  commentState,
+  setCommentState,
+}) => {
+  const { id, created_at, updated_at, likes, author, body } = comment;
+
+  const deleteComment = (id) => {
+    apiCall
+      .delete(`/api/v1/poll_comments/${id}`)
       .then(() => document.location.reload());
   };
 
@@ -337,6 +485,94 @@ export const CommentField = ({ pollId, commentState, setCommentState }) => {
           }}
           type="button"
           id="cancel-editing"
+          onClick={cancelEditing}
+        >
+          Cancel
+        </button>
+        {isSuccess && (
+          <p
+            className="message-success mt-3"
+            style={{ textAlign: "center", color: "#964202" }}
+          >
+            Your comment has been saved successfully. To see it, reload the
+            page.
+          </p>
+        )}
+      </form>
+    </>
+  );
+};
+
+export const NestedCommentField = ({ pollId, commentId }) => {
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState({ body: "" });
+  const [commentContent, setCommentContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    apiCall
+      .post("/api/v1/poll_comments/", {
+        poll_id: pollId,
+        poll_comment_id: commentId,
+        body: commentContent,
+      })
+      .then(() => {
+        setIsSuccess(true);
+        setTimeout(() => {
+          setIsSuccess(false);
+        }, 3000);
+        setCommentContent("");
+        setErrorMessage({ body: "" });
+      })
+      .catch((error) => {
+        setErrorMessage({ ...error.response.data });
+        console.log(error.response.data);
+      });
+  };
+
+  const cancelEditing = (e) => {
+    setCommentContent("");
+    e.target.style.display = "none";
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <p
+          className="answer-note nested-comment"
+          id={`your-poll-comment-to${commentId}`}
+          style={{ marginTop: "1rem" }}
+        >
+          Your Comment
+        </p>
+        <RichTextField
+          descriptionContent={""}
+          setDescriptionContent={setCommentContent}
+        />
+        {errorMessage?.body && <ErrorMessage message={errorMessage.body} />}
+        <button
+          className="btn btn-default link-button"
+          style={{
+            marginTop: "0",
+            padding: "10px 30px",
+            fontSize: "1.4rem",
+          }}
+          type="submit"
+          id={`blog-poll-to-comment${commentId}`}
+        >
+          Comment
+        </button>
+        <button
+          className="btn btn-default link-button"
+          style={{
+            marginTop: "0",
+            marginLeft: "10px",
+            padding: "10px 30px",
+            fontSize: "1.4rem",
+            display: "none",
+          }}
+          type="button"
+          id={`cancel-editing-to-poll-comment${commentId}`}
           onClick={cancelEditing}
         >
           Cancel
